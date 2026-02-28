0f95107a345abf1b1f52f769be725297fc3d6e89
Jerômimo Neto <95920674+JEROON1MO@users.noreply.github.com>
2026-02-28 08:41:17 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,14 +6,23 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [
     // The React and Tailwind plugins are both required for Make, even if
-    // Tailwind is not being actively used – do not remove them
-    react(),
+    // Tailwind is not being actively used - do not remove them
+    react({
+      jsxRuntime: 'automatic',
+    }),
     tailwindcss(),
   ],
+  base: '/Portfoliowebsitedesign/',
   resolve: {
     alias: {
       // Alias @ to the src directory
       '@': path.resolve(__dirname, './src'),
+      'react/jsx-runtime': 'react/jsx-runtime.js',
+    },
+  },
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
     },
   },
 
